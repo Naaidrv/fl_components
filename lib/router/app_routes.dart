b1954bce6894028207a9ace2196b3186fa9796e6
Naaidrv <zabriverav@gmail.com>
2023-02-06 17:14:54 -0600

    Add: Routes - Part 2

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -1,8 +1,17 @@
+import 'package:components/models/models.dart';
 import 'package:components/screens/screens.dart';
 import 'package:flutter/material.dart';
 
 class AppRoutes {
   static const initialRoute = 'Home';
+
+  static final menuOptions = <MenuOption>[
+    MenuOption(route: 'Home', name: 'Home Screen', screen: const HomeScreen(), icon: Icons.home_filled),
+    MenuOption(route: 'ListView1', name: 'ListViewOne', screen: const ListView1Screen(), icon: Icons.list),
+    MenuOption(route: 'ListView2', name: 'ListViewTwo', screen: const ListView2Screen(), icon: Icons.list_alt_outlined),
+    MenuOption(route: 'AlertScreen', name: 'AlertScreen', screen: const AlertScreen(), icon: Icons.add_alert),
+    MenuOption(route: 'CardScreen', name: 'Card', screen: const CardScreen(), icon: Icons.credit_card),
+  ];
 
   static Map<String, Widget Function(BuildContext)> routes = {
     'Home': (BuildContext context) => const HomeScreen(),
@@ -13,8 +22,8 @@ class AppRoutes {
   };
 
   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
-        return MaterialPageRoute(
-          builder: (context) => const AlertScreen(),
-        );
-      }
+    return MaterialPageRoute(
+      builder: (context) => const AlertScreen(),
+    );
+  }
 }
